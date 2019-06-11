05811fc9f11ef27152f710f62c5878c6dbc8e037
Daniel Alfonso Jiménez Suárez <kanakemandatos@gmail.com>
2019-06-11 14:39:59 -0500

    login movil no name route

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', 'LoginController@login')->name("loginMovil");
+Route::post('login', 'LoginController@login');
 
